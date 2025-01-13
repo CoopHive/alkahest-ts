92e6a8f016785e3c88b2d5e9d721328a96190c2e
疒奀 <yz@mmon.co>
2025-01-14 01:29:09 +0800

    flatten natural bundle type

--- src/utils.ts
+++ src/utils.ts
@@ -9,6 +9,7 @@ import {
 } from "viem";
 import { abi as iEasAbi } from "./contracts/IEAS";
 import { type Account, type Chain } from "viem";
+import type { TokenBundle, TokenBundleFlat } from "./types";
 
 export type ViemClient = WalletClient<Transport, Chain, Account> &
   PublicActions;
@@ -32,3 +33,13 @@ export const getAttestationFromTxHash = async (
     logs: tx.logs,
   })[0].args;
 };
+
+export const flattenTokenBundle = (bundle: TokenBundle): TokenBundleFlat => ({
+  erc20Tokens: bundle.erc20s.map((x) => x.token),
+  erc20Amounts: bundle.erc20s.map((x) => x.amount),
+  erc721Tokens: bundle.erc721s.map((x) => x.token),
+  erc721TokenIds: bundle.erc721s.map((x) => x.tokenId),
+  erc1155Tokens: bundle.erc1155s.map((x) => x.token),
+  erc1155TokenIds: bundle.erc1155s.map((x) => x.tokenId),
+  erc1155Amounts: bundle.erc1155s.map((x) => x.amount),
+});
