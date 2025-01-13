5ba4088c2793ac36405533a52e09c2fbb091f6ba
疒奀 <yz@mmon.co>
2025-01-14 01:46:18 +0800

    erc1155 to parity with erc721

--- src/clients/erc1155.ts
+++ src/clients/erc1155.ts
@@ -1,25 +1,117 @@
-import { getAttestationFromTxHash, type ViemClient } from "../utils";
-
-import { abi as erc1155BarterUtilsAbi } from "../contracts/ERC1155BarterUtils";
+import {
+  flattenTokenBundle,
+  getAttestationFromTxHash,
+  type ViemClient,
+} from "../utils";
 import { contractAddresses } from "../config";
+import type {
+  ApprovalPurpose,
+  Demand,
+  Erc1155,
+  Erc20,
+  Erc721,
+  TokenBundle,
+} from "../types";
+
+import { abi as erc1155BarterUtilsAbi } from "../contracts/ERC1155BarterCrossToken";
+import { abi as erc1155EscrowAbi } from "../contracts/ERC1155EscrowObligation";
+import { abi as erc1155PaymentAbi } from "../contracts/ERC1155PaymentObligation";
+import { abi as erc1155Abi } from "../contracts/IERC1155";
 
 export const makeErc1155Client = (viemClient: ViemClient) => ({
+  approveAll: async (
+    token_contract: `0x${string}`,
+    purpose: ApprovalPurpose,
+  ) => {
+    const to =
+      purpose === "escrow"
+        ? contractAddresses[viemClient.chain.name].erc1155EscrowObligation
+        : contractAddresses[viemClient.chain.name].erc1155PaymentObligation;
+    const hash = await viemClient.writeContract({
+      address: token_contract,
+      abi: erc1155Abi.abi,
+      functionName: "setApprovalForAll",
+      args: [to, true],
+    });
+
+    return hash;
+  },
+
+  revokeAll: async (
+    token_contract: `0x${string}`,
+    purpose: ApprovalPurpose,
+  ) => {
+    const to =
+      purpose === "escrow"
+        ? contractAddresses[viemClient.chain.name].erc1155EscrowObligation
+        : contractAddresses[viemClient.chain.name].erc1155PaymentObligation;
+    const hash = await viemClient.writeContract({
+      address: token_contract,
+      abi: erc1155Abi.abi,
+      functionName: "setApprovalForAll",
+      args: [to, false],
+    });
+
+    return hash;
+  },
+
+  buyWithErc1155: async (price: Erc1155, item: Demand, expiration: bigint) => {
+    const hash = await viemClient.writeContract({
+      address: contractAddresses[viemClient.chain.name].erc1155EscrowObligation,
+      abi: erc1155EscrowAbi.abi,
+      functionName: "makeStatement",
+      args: [
+        {
+          token: price.address,
+          tokenId: price.id,
+          amount: price.value,
+          arbiter: item.arbiter,
+          demand: item.demand,
+        },
+        expiration,
+      ],
+    });
+
+    const attested = await getAttestationFromTxHash(viemClient, hash);
+    return { hash, attested };
+  },
+
+  payWithErc1155: async (price: Erc1155, payee: `0x${string}`) => {
+    const hash = await viemClient.writeContract({
+      address:
+        contractAddresses[viemClient.chain.name].erc1155PaymentObligation,
+      abi: erc1155PaymentAbi.abi,
+      functionName: "makeStatement",
+      args: [
+        {
+          token: price.address,
+          tokenId: price.id,
+          amount: price.value,
+          payee,
+        },
+      ],
+    });
+
+    const attested = await getAttestationFromTxHash(viemClient, hash);
+    return { hash, attested };
+  },
+
   buyErc1155ForErc1155: async (
-    bid: { token: `0x${string}`; tokenId: bigint; amount: bigint },
-    ask: { token: `0x${string}`; tokenId: bigint; amount: bigint },
-    expiration: bigint = 0n,
+    bid: Erc1155,
+    ask: Erc1155,
+    expiration: bigint,
   ) => {
     const hash = await viemClient.writeContract({
       address: contractAddresses[viemClient.chain.name].erc1155BarterUtils,
       abi: erc1155BarterUtilsAbi.abi,
       functionName: "buyErc1155ForErc1155",
       args: [
-        bid.token,
-        bid.tokenId,
-        bid.amount,
-        ask.token,
-        ask.tokenId,
-        ask.amount,
+        bid.address,
+        bid.id,
+        bid.value,
+        ask.address,
+        ask.id,
+        ask.value,
         expiration,
       ],
     });
@@ -36,5 +128,62 @@ export const makeErc1155Client = (viemClient: ViemClient) => ({
     });
     const tx = await viemClient.waitForTransactionReceipt({ hash });
     return { hash };
+  },
+
+  buyErc20WithErc1155: async (bid: Erc1155, ask: Erc20, expiration: bigint) => {
+    const hash = await viemClient.writeContract({
+      address: contractAddresses[viemClient.chain.name].erc1155BarterUtils,
+      abi: erc1155BarterUtilsAbi.abi,
+      functionName: "buyErc20WithErc1155",
+      args: [
+        bid.address,
+        bid.id,
+        bid.value,
+        ask.address,
+        ask.value,
+        expiration,
+      ],
+    });
+
+    const attested = await getAttestationFromTxHash(viemClient, hash);
+    return { hash, attested };
+  },
+
+  buyErc1155WithErc721: async (
+    bid: Erc1155,
+    ask: Erc721,
+    expiration: bigint,
+  ) => {
+    const hash = await viemClient.writeContract({
+      address: contractAddresses[viemClient.chain.name].erc1155BarterUtils,
+      abi: erc1155BarterUtilsAbi.abi,
+      functionName: "buyErc721WithErc1155",
+      args: [bid.address, bid.id, bid.value, ask.address, ask.id, expiration],
+    });
+    const attested = await getAttestationFromTxHash(viemClient, hash);
+    return { hash, attested };
+  },
+
+  buyBundleWithErc1155: async (
+    bid: Erc1155,
+    ask: TokenBundle,
+    payee: `0x${string}`,
+    expiration: bigint,
+  ) => {
+    const hash = await viemClient.writeContract({
+      address: contractAddresses[viemClient.chain.name].erc1155BarterUtils,
+      abi: erc1155BarterUtilsAbi.abi,
+      functionName: "buyBundleWithErc1155",
+      args: [
+        bid.address,
+        bid.id,
+        bid.value,
+        { ...flattenTokenBundle(ask), payee },
+        expiration,
+      ],
+    });
+
+    const attested = await getAttestationFromTxHash(viemClient, hash);
+    return { hash, attested };
   },
 });
