a24f35a18a89c2b4588d7008b81e4498475deb67
疒奀 <yz@mmon.co>
2025-02-21 19:42:03 +0800

    docstrings

--- src/clients/arbiters.ts
+++ src/clients/arbiters.ts
@@ -1,8 +1,8 @@
 import {
-  decodeAbiParameters,
-  encodeAbiParameters,
-  parseAbiItem,
-  parseAbiParameters,
+	decodeAbiParameters,
+	encodeAbiParameters,
+	parseAbiItem,
+	parseAbiParameters,
 } from "viem";
 import type { ViemClient } from "../utils";
 
@@ -10,87 +10,130 @@ import { abi as trustedOracleArbiterAbi } from "../contracts/TrustedOracleArbite
 import { contractAddresses } from "../config";
 
 export const makeArbitersClient = (viemClient: ViemClient) => ({
-  encodeTrustedPartyDemand: (demand: {
-    creator: `0x${string}`;
-    baseArbiter: `0x${string}`;
-    baseDemand: `0x${string}`;
-  }) => {
-    return encodeAbiParameters(
-      parseAbiParameters(
-        "(address creator, address baseArbiter, bytes baseDemand)",
-      ),
-      [demand],
-    );
-  },
-  decodeTrustedPartyDemand: (demandData: `0x${string}`) => {
-    return decodeAbiParameters(
-      parseAbiParameters(
-        "(address creator, address baseArbiter, bytes baseDemand)",
-      ),
-      demandData,
-    )[0];
-  },
-  encodeSpecificAttestationDemand: (demand: { uid: `0x${string}` }) => {
-    return encodeAbiParameters(parseAbiParameters("(bytes32 uid)"), [demand]);
-  },
-  decodeSpecificAttestationDemand: (demandData: `0x${string}`) => {
-    return decodeAbiParameters(
-      parseAbiParameters("(bytes32 uid)"),
-      demandData,
-    )[0];
-  },
-  encodeTrustedOracleDemand: (demand: { oracle: `0x${string}` }) => {
-    return encodeAbiParameters(parseAbiParameters("(address oracle)"), [
-      demand,
-    ]);
-  },
-  decodeTrustedOracleDemand: (demandData: `0x${string}`) => {
-    return decodeAbiParameters(
-      parseAbiParameters("(address oracle)"),
-      demandData,
-    )[0];
-  },
-  arbitrateAsTrustedOracle: async (
-    statement: `0x${string}`,
-    decision: boolean,
-  ) => {
-    const hash = await viemClient.writeContract({
-      address: contractAddresses[viemClient.chain.name].trustedOracleArbiter,
-      abi: trustedOracleArbiterAbi.abi,
-      functionName: "arbitrate",
-      args: [statement, decision],
-    });
-    return hash;
-  },
-  waitForTrustedOracleArbitration: async (
-    oracle: `0x${string}`,
-    statement: `0x${string}`,
-  ) => {
-    const event = parseAbiItem(
-      "event ArbitrationMade(address indexed oracle, bytes32 indexed statement, bool decision)",
-    );
-    const logs = await viemClient.getLogs({
-      address: contractAddresses[viemClient.chain.name].trustedOracleArbiter,
-      event,
-      args: { oracle, statement },
-      fromBlock: "earliest",
-      toBlock: "latest",
-    });
+	/**
+	 * Encodes TrustedPartyArbiter.DemandData to bytes.
+	 * @param demand - struct DemandData {address creator, address baseArbiter, bytes baseDemand}
+	 * @returns abi encoded bytes
+	 */
+	encodeTrustedPartyDemand: (demand: {
+		creator: `0x${string}`;
+		baseArbiter: `0x${string}`;
+		baseDemand: `0x${string}`;
+	}) => {
+		return encodeAbiParameters(
+			parseAbiParameters(
+				"(address creator, address baseArbiter, bytes baseDemand)",
+			),
+			[demand],
+		);
+	},
+	/**
+	 * Decodes TrustedPartyArbiter.DemandData from bytes.
+	 * @param demandData - DemandData as abi encoded bytes
+	 * @returns the decoded DemandData object
+	 */
+	decodeTrustedPartyDemand: (demandData: `0x${string}`) => {
+		return decodeAbiParameters(
+			parseAbiParameters(
+				"(address creator, address baseArbiter, bytes baseDemand)",
+			),
+			demandData,
+		)[0];
+	},
+	/**
+	 * Encodes SpecificAttestationArbiter.DemandData to bytes.
+	 * @param demand - struct DemandData {bytes32 uid}
+	 * @returns abi encoded bytes
+	 */
+	encodeSpecificAttestationDemand: (demand: { uid: `0x${string}` }) => {
+		return encodeAbiParameters(parseAbiParameters("(bytes32 uid)"), [demand]);
+	},
+	/**
+	 * Decodes SpecificAttestationArbiter.DemandData from bytes.
+	 * @param demandData - DemandData as abi encoded bytes
+	 * @returns the decoded DemandData object
+	 */
+	decodeSpecificAttestationDemand: (demandData: `0x${string}`) => {
+		return decodeAbiParameters(
+			parseAbiParameters("(bytes32 uid)"),
+			demandData,
+		)[0];
+	},
+	/**
+	 * Encodes TrustedOracleArbiter.DemandData to bytes.
+	 * @param demand - struct DemandData {address oracle}
+	 * @returns abi encoded bytes
+	 */
+	encodeTrustedOracleDemand: (demand: { oracle: `0x${string}` }) => {
+		return encodeAbiParameters(parseAbiParameters("(address oracle)"), [
+			demand,
+		]);
+	},
+	/**
+	 * Decodes TrustedOracleArbiter.DemandData from bytes.
+	 * @param demandData - DemandData as abi encoded bytes
+	 * @returns the decoded DemandData object
+	 */
+	decodeTrustedOracleDemand: (demandData: `0x${string}`) => {
+		return decodeAbiParameters(
+			parseAbiParameters("(address oracle)"),
+			demandData,
+		)[0];
+	},
+	/**
+	 * Arbitrate on the validality of an obligation fulfilling
+	 * a demand for TrustedOracleArbiter
+	 * @param statement - bytes32 statement
+	 * @param decision - bool decision
+	 * @returns transaction hash
+	 */
+	arbitrateAsTrustedOracle: async (
+		statement: `0x${string}`,
+		decision: boolean,
+	) => {
+		const hash = await viemClient.writeContract({
+			address: contractAddresses[viemClient.chain.name].trustedOracleArbiter,
+			abi: trustedOracleArbiterAbi.abi,
+			functionName: "arbitrate",
+			args: [statement, decision],
+		});
+		return hash;
+	},
+	/**
+	 * Wait for an arbitration to be made on a TrustedOracleArbiter
+	 * @param oracle - address of the oracle
+	 * @param statement - bytes32 statement uid
+	 * @returns the event args
+	 */
+	waitForTrustedOracleArbitration: async (
+		oracle: `0x${string}`,
+		statement: `0x${string}`,
+	) => {
+		const event = parseAbiItem(
+			"event ArbitrationMade(address indexed oracle, bytes32 indexed statement, bool decision)",
+		);
+		const logs = await viemClient.getLogs({
+			address: contractAddresses[viemClient.chain.name].trustedOracleArbiter,
+			event,
+			args: { oracle, statement },
+			fromBlock: "earliest",
+			toBlock: "latest",
+		});
 
-    if (logs.length) return logs[0].args;
+		if (logs.length) return logs[0].args;
 
-    return new Promise((resolve) => {
-      const unwatch = viemClient.watchEvent({
-        address: contractAddresses[viemClient.chain.name].trustedOracleArbiter,
-        event,
-        args: { oracle, statement },
-        pollingInterval: 1000,
-        onLogs: (logs) => {
-          resolve(logs[0].args);
-          unwatch();
-        },
-        fromBlock: 1n,
-      });
-    });
-  },
+		return new Promise((resolve) => {
+			const unwatch = viemClient.watchEvent({
+				address: contractAddresses[viemClient.chain.name].trustedOracleArbiter,
+				event,
+				args: { oracle, statement },
+				pollingInterval: 1000,
+				onLogs: (logs) => {
+					resolve(logs[0].args);
+					unwatch();
+				},
+				fromBlock: 1n,
+			});
+		});
+	},
 });
