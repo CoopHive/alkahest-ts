6a3d6c6cf7935db190837f62f06b680a8a2c4262
疒奀 <yz@mmon.co>
2025-02-21 18:46:43 +0800

    encode/decode

--- src/clients/arbiters.ts
+++ src/clients/arbiters.ts
@@ -1,29 +1,62 @@
 import {
-  decodeAbiParameters,
-  encodeAbiParameters,
-  parseAbiParameters,
+	decodeAbiParameters,
+	encodeAbiParameters,
+	parseAbiParameters,
 } from "viem";
 import type { ViemClient } from "../utils";
 
 export const makeArbitersClient = (viemClient: ViemClient) => ({
-  encodeTrustedPartyDemand: (demand: {
-    creator: `0x${string}`;
-    baseArbiter: `0x${string}`;
-    baseDemand: `0x${string}`;
-  }) => {
-    return encodeAbiParameters(
-      parseAbiParameters(
-        "(address creator, address baseArbiter, bytes baseDemand)",
-      ),
-      [demand],
-    );
-  },
-  decodeTrustedPartyDemand: (demandData: `0x${string}`) => {
-    return decodeAbiParameters(
-      parseAbiParameters(
-        "(address creator, address baseArbiter, bytes baseDemand)",
-      ),
-      demandData,
-    )[0];
-  },
+	encodeTrustedPartyDemand: (demand: {
+		creator: `0x${string}`;
+		baseArbiter: `0x${string}`;
+		baseDemand: `0x${string}`;
+	}) => {
+		return encodeAbiParameters(
+			parseAbiParameters(
+				"(address creator, address baseArbiter, bytes baseDemand)",
+			),
+			[demand],
+		);
+	},
+	decodeTrustedPartyDemand: (demandData: `0x${string}`) => {
+		return decodeAbiParameters(
+			parseAbiParameters(
+				"(address creator, address baseArbiter, bytes baseDemand)",
+			),
+			demandData,
+		)[0];
+	},
+	encodeSpecificAttestationDemand: (demand: {
+		uid: `0x${string}`;
+	}) => {
+		return encodeAbiParameters(parseAbiParameters("(bytes32 uid)"), [demand]);
+	},
+	decodeSpecificAttestationDemand: (demandData: `0x${string}`) => {
+		return decodeAbiParameters(
+			parseAbiParameters("(bytes32 uid)"),
+			demandData,
+		)[0];
+	},
+	encodeTrustedOracleDemand: (demand: {
+		oracle: `0x${string}`;
+	}) => {
+		return encodeAbiParameters(parseAbiParameters("(address oracle)"), [
+			demand,
+		]);
+	},
+	decodeTrustedOracleDemand: (demandData: `0x${string}`) => {
+		return decodeAbiParameters(
+			parseAbiParameters("(address oracle)"),
+			demandData,
+		)[0];
+	},
+	arbitrateAsTrustedOracle: async (
+		statement: `0x${string}`,
+		decision: boolean,
+	) => {},
+	waitForTrustedOracleArbitration: async (
+		oracle: `0x${string}`,
+		statement: `0x${string}`,
+		fromBlock?: number,
+	) => {},
 });
