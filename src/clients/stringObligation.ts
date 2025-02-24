d74ab8b01baf245ef91cc810adee11b84ee15e49
疒奀 <yz@mmon.co>
2025-02-25 00:28:37 +0800

    any to unknown

--- src/clients/stringObligation.ts
+++ src/clients/stringObligation.ts
@@ -7,62 +7,62 @@ import type { z } from "zod";
 import type { Type } from "arktype";
 
 export const makeStringObligationClient = (viemClient: ViemClient) => {
-  const decode = (statementData: `0x${string}`) => {
-    return decodeAbiParameters(
-      parseAbiParameters("(string item)"),
-      statementData,
-    )[0];
-  };
+	const decode = (statementData: `0x${string}`) => {
+		return decodeAbiParameters(
+			parseAbiParameters("(string item)"),
+			statementData,
+		)[0];
+	};
 
-  const makeStatement = async (item: string, refUid?: `0x${string}`) =>
-    await viemClient.writeContract({
-      address: contractAddresses["Base Sepolia"].stringObligation,
-      abi: stringObligationAbi.abi,
-      functionName: "makeStatement",
-      args: [
-        { item },
-        refUid ??
-          "0x0000000000000000000000000000000000000000000000000000000000000000", // bytes32 0
-      ],
-    });
+	const makeStatement = async (item: string, refUid?: `0x${string}`) =>
+		await viemClient.writeContract({
+			address: contractAddresses["Base Sepolia"].stringObligation,
+			abi: stringObligationAbi.abi,
+			functionName: "makeStatement",
+			args: [
+				{ item },
+				refUid ??
+					"0x0000000000000000000000000000000000000000000000000000000000000000", // bytes32 0
+			],
+		});
 
-  const getZodParseFunc = (opts: { async: boolean; safe: boolean }) => {
-    let command = opts.safe ? "safeParse" : "parse";
-    if (opts.async) {
-      command += "Async";
-    }
-    return command as "parse" | "parseAsync" | "safeParse" | "safeParseAsync";
-  };
+	const getZodParseFunc = (opts: { async: boolean; safe: boolean }) => {
+		let command = opts.safe ? "safeParse" : "parse";
+		if (opts.async) {
+			command += "Async";
+		}
+		return command as "parse" | "parseAsync" | "safeParse" | "safeParseAsync";
+	};
 
-  return {
-    decode,
-    decodeJson: <T>(statementData: `0x${string}`) => {
-      const decoded = decode(statementData);
-      return JSON.parse(decoded.item) as T;
-    },
-    decodeZod: (
-      statementData: `0x${string}`,
-      schema: z.ZodSchema<any, any, any>,
-      schemaOptions?: Partial<z.ParseParams>,
-      parseOptions: {
-        async: boolean;
-        safe: boolean;
-      } = { async: false, safe: false },
-    ): z.infer<typeof schema> => {
-      return schema[getZodParseFunc(parseOptions)](
-        decode(statementData),
-        schemaOptions,
-      );
-    },
-    decodeArkType: <Schema extends Type<any, any>>(
-      statementData: `0x${string}`,
-      schema: Schema,
-    ): Schema["inferOut"] => {
-      return schema(decode(statementData));
-    },
-    makeStatement,
-    makeStatementJson: async <T>(item: T, refUid?: `0x${string}`) => {
-      return await makeStatement(JSON.stringify(item), refUid);
-    },
-  };
+	return {
+		decode,
+		decodeJson: <T>(statementData: `0x${string}`) => {
+			const decoded = decode(statementData);
+			return JSON.parse(decoded.item) as T;
+		},
+		decodeZod: (
+			statementData: `0x${string}`,
+			schema: z.ZodSchema<unknown, unknown, unknown>,
+			schemaOptions?: Partial<z.ParseParams>,
+			parseOptions: {
+				async: boolean;
+				safe: boolean;
+			} = { async: false, safe: false },
+		): z.infer<typeof schema> => {
+			return schema[getZodParseFunc(parseOptions)](
+				decode(statementData),
+				schemaOptions,
+			);
+		},
+		decodeArkType: <Schema extends Type<unknown, unknown>>(
+			statementData: `0x${string}`,
+			schema: Schema,
+		): Schema["inferOut"] => {
+			return schema(decode(statementData));
+		},
+		makeStatement,
+		makeStatementJson: async <T>(item: T, refUid?: `0x${string}`) => {
+			return await makeStatement(JSON.stringify(item), refUid);
+		},
+	};
 };
