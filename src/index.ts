83952d684832c287823e55287b5fdc5ac7c33783
疒奀 <yz@mmon.co>
2025-02-24 23:29:49 +0800

    string obligatio

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import {
-	parseAbiItem,
-	parseEventLogs,
-	publicActions,
-	type Account,
-	type Chain,
-	type Transport,
-	type WalletClient,
+  parseAbiItem,
+  parseEventLogs,
+  publicActions,
+  type Account,
+  type Chain,
+  type Transport,
+  type WalletClient,
 } from "viem";
 import { contractAddresses, supportedChains } from "./config";
 import { makeErc20Client } from "./clients/erc20";
@@ -13,6 +13,7 @@ import { makeErc721Client } from "./clients/erc721";
 import { makeErc1155Client } from "./clients/erc1155";
 import { makeTokenBundleClient } from "./clients/tokenBundle";
 import { makeAttestationClient } from "./clients/attestation";
+import { makeStringObligationClient } from "./clients/stringObligation";
 
 import { abi as easAbi } from "./contracts/IEAS";
 import { makeArbitersClient } from "./clients/arbiters";
@@ -37,119 +38,122 @@ import { makeArbitersClient } from "./clients/arbiters";
  * ```
  */
 export const makeClient = (
-	walletClient: WalletClient<Transport, Chain, Account>,
+  walletClient: WalletClient<Transport, Chain, Account>,
 ) => {
-	if (!supportedChains.includes(walletClient.chain.name)) {
-		throw new Error("unsupported chain");
-	}
+  if (!supportedChains.includes(walletClient.chain.name)) {
+    throw new Error("unsupported chain");
+  }
 
-	const viemClient = walletClient.extend(publicActions);
+  const viemClient = walletClient.extend(publicActions);
 
-	return {
-		/** Methods for interacting with Arbiters */
-		arbiters: makeArbitersClient(viemClient),
+  return {
+    /** Methods for interacting with Arbiters */
+    arbiters: makeArbitersClient(viemClient),
 
-		/** Methods for interacting with ERC20 tokens */
-		erc20: makeErc20Client(viemClient),
+    /** Methods for interacting with ERC20 tokens */
+    erc20: makeErc20Client(viemClient),
 
-		/** Methods for interacting with ERC721 tokens */
-		erc721: makeErc721Client(viemClient),
+    /** Methods for interacting with ERC721 tokens */
+    erc721: makeErc721Client(viemClient),
 
-		/** Methods for interacting with ERC1155 tokens */
-		erc1155: makeErc1155Client(viemClient),
+    /** Methods for interacting with ERC1155 tokens */
+    erc1155: makeErc1155Client(viemClient),
 
-		/** Methods for interacting with token bundles */
-		bundle: makeTokenBundleClient(viemClient),
+    /** Methods for interacting with token bundles */
+    bundle: makeTokenBundleClient(viemClient),
 
-		/** Methods for interacting with attestations */
-		attestation: makeAttestationClient(viemClient),
+    /** Methods for interacting with attestations */
+    attestation: makeAttestationClient(viemClient),
 
-		/** The underlying Viem client */
-		viemClient,
+    /** Utilities for StringObligation */
+    stringResult: makeStringObligationClient(viemClient),
 
-		/** Address of the account used to create this client */
-		address: viemClient.account.address,
+    /** The underlying Viem client */
+    viemClient,
 
-		/**
-		 * Retrieves an attestation by its UID
-		 * @param uid - The unique identifier of the attestation
-		 * @returns The attestation data
-		 */
-		getAttestation: async (uid: `0x${string}`) => {
-			const attestation = await viemClient.readContract({
-				address: contractAddresses[viemClient.chain.name].eas,
-				abi: easAbi.abi,
-				functionName: "getAttestation",
-				args: [uid],
-			});
-			return attestation;
-		},
+    /** Address of the account used to create this client */
+    address: viemClient.account.address,
 
-		/**
-		 * Gets an attestation from a transaction hash
-		 * @param hash - The transaction hash
-		 * @returns The attestation event args
-		 */
-		getAttestationFromTxHash: async (hash: `0x${string}`) => {
-			const tx = await viemClient.waitForTransactionReceipt({ hash });
-			return parseEventLogs({
-				abi: easAbi.abi,
-				eventName: "Attested",
-				logs: tx.logs,
-			})[0].args;
-		},
+    /**
+     * Retrieves an attestation by its UID
+     * @param uid - The unique identifier of the attestation
+     * @returns The attestation data
+     */
+    getAttestation: async (uid: `0x${string}`) => {
+      const attestation = await viemClient.readContract({
+        address: contractAddresses[viemClient.chain.name].eas,
+        abi: easAbi.abi,
+        functionName: "getAttestation",
+        args: [uid],
+      });
+      return attestation;
+    },
 
-		/**
-		 * Waits for an escrow to be fulfilled
-		 * @param contractAddress - The address of the escrow contract
-		 * @param buyAttestation - The UID of the buy attestation
-		 * @returns Object containing payment, fulfillment and fulfiller details
-		 *
-		 * @example
-		 * ```ts
-		 * // Wait for fulfillment of an escrow
-		 * const fulfillment = await client.waitForFulfillment(
-		 *   contractAddresses["Base Sepolia"].erc20EscrowObligation,
-		 *   escrow.attested.uid,
-		 * );
-		 * ```
-		 */
-		waitForFulfillment: async (
-			contractAddress: `0x${string}`,
-			buyAttestation: `0x${string}`,
-		): Promise<{
-			payment?: `0x${string}` | undefined;
-			fulfillment?: `0x${string}` | undefined;
-			fulfiller?: `0x${string}` | undefined;
-		}> => {
-			const fulfillmentEvent = parseAbiItem(
-				"event EscrowClaimed(bytes32 indexed payment, bytes32 indexed fulfillment, address indexed fulfiller)",
-			);
-			const logs = await viemClient.getLogs({
-				address: contractAddress,
-				event: fulfillmentEvent,
-				args: { payment: buyAttestation },
-				fromBlock: "earliest",
-				toBlock: "latest",
-			});
+    /**
+     * Gets an attestation from a transaction hash
+     * @param hash - The transaction hash
+     * @returns The attestation event args
+     */
+    getAttestationFromTxHash: async (hash: `0x${string}`) => {
+      const tx = await viemClient.waitForTransactionReceipt({ hash });
+      return parseEventLogs({
+        abi: easAbi.abi,
+        eventName: "Attested",
+        logs: tx.logs,
+      })[0].args;
+    },
 
-			if (logs.length) return logs[0].args;
+    /**
+     * Waits for an escrow to be fulfilled
+     * @param contractAddress - The address of the escrow contract
+     * @param buyAttestation - The UID of the buy attestation
+     * @returns Object containing payment, fulfillment and fulfiller details
+     *
+     * @example
+     * ```ts
+     * // Wait for fulfillment of an escrow
+     * const fulfillment = await client.waitForFulfillment(
+     *   contractAddresses["Base Sepolia"].erc20EscrowObligation,
+     *   escrow.attested.uid,
+     * );
+     * ```
+     */
+    waitForFulfillment: async (
+      contractAddress: `0x${string}`,
+      buyAttestation: `0x${string}`,
+    ): Promise<{
+      payment?: `0x${string}` | undefined;
+      fulfillment?: `0x${string}` | undefined;
+      fulfiller?: `0x${string}` | undefined;
+    }> => {
+      const fulfillmentEvent = parseAbiItem(
+        "event EscrowClaimed(bytes32 indexed payment, bytes32 indexed fulfillment, address indexed fulfiller)",
+      );
+      const logs = await viemClient.getLogs({
+        address: contractAddress,
+        event: fulfillmentEvent,
+        args: { payment: buyAttestation },
+        fromBlock: "earliest",
+        toBlock: "latest",
+      });
 
-			return new Promise((resolve) => {
-				const unwatch = viemClient.watchEvent({
-					address: contractAddress,
-					event: fulfillmentEvent,
-					args: { payment: buyAttestation },
-					pollingInterval: 1000,
-					onLogs: (logs) => {
-						resolve(logs[0].args);
-						unwatch();
-					},
-					fromBlock: 1n,
-				});
-			});
-		},
-	};
+      if (logs.length) return logs[0].args;
+
+      return new Promise((resolve) => {
+        const unwatch = viemClient.watchEvent({
+          address: contractAddress,
+          event: fulfillmentEvent,
+          args: { payment: buyAttestation },
+          pollingInterval: 1000,
+          onLogs: (logs) => {
+            resolve(logs[0].args);
+            unwatch();
+          },
+          fromBlock: 1n,
+        });
+      });
+    },
+  };
 };
 
 export * from "./types";
