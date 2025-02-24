a209ef079a6422750e90d7640401df17ebc88c97
疒奀 <yz@mmon.co>
2025-02-24 23:51:31 +0800

    zod & arktype decode

--- src/clients/stringObligation.ts
+++ src/clients/stringObligation.ts
@@ -3,6 +3,8 @@ import type { ViemClient } from "../utils";
 
 import { abi as stringObligationAbi } from "../contracts/StringObligation";
 import { contractAddresses } from "../config";
+import type { z } from "zod";
+import type { Type } from "arktype";
 
 export const makeStringObligationClient = (viemClient: ViemClient) => {
   const decode = (statementData: `0x${string}`) => {
@@ -12,13 +14,41 @@ export const makeStringObligationClient = (viemClient: ViemClient) => {
     )[0];
   };
 
+  const getZodParseFunc = (opts: { async: boolean; safe: boolean }) => {
+    let command = opts.safe ? "safeParse" : "parse";
+    if (opts.async) {
+      command += "Async";
+    }
+    return command as "parse" | "parseAsync" | "safeParse" | "safeParseAsync";
+  };
+
   return {
     decode,
     decodeJson: <T>(statementData: `0x${string}`) => {
       const decoded = decode(statementData);
       return JSON.parse(decoded.item) as T;
     },
-    makeStatement: async (item: string, refUid: `0x${string}` | undefined) => {
+    decodeZod: (
+      statementData: `0x${string}`,
+      schema: z.ZodSchema<any, any, any>,
+      schemaOptions?: Partial<z.ParseParams>,
+      parseOptions: {
+        async: boolean;
+        safe: boolean;
+      } = { async: false, safe: false },
+    ): z.infer<typeof schema> => {
+      return schema[getZodParseFunc(parseOptions)](
+        decode(statementData),
+        schemaOptions,
+      );
+    },
+    decodeArkType: <Schema extends Type<any, any>>(
+      statementData: `0x${string}`,
+      schema: Schema,
+    ): Schema["inferOut"] => {
+      return schema(decode(statementData));
+    },
+    makeStatement: async (item: string, refUid?: `0x${string}`) => {
       return await viemClient.writeContract({
         address: contractAddresses["Base Sepolia"].stringObligation,
         abi: stringObligationAbi.abi,
