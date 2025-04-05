7ee66114103c70eefc9acb1a3d24595a0a153828
疒奀 <yz@mmon.co>
2025-04-05 21:18:00 +0800

    arbitrate over past logs

--- src/oracle/oracle.ts
+++ src/oracle/oracle.ts
@@ -1,13 +1,17 @@
 import {
+  decodeAbiParameters,
   type AbiEvent,
   type AbiParameter,
   type BlockNumber,
   type BlockTag,
   type DecodeAbiParametersReturnType,
   type GetLogsParameters,
+  type GetLogsReturnType,
 } from "viem";
 import type { ChainAddresses } from "../types";
-import type { ViemClient } from "../utils";
+import { getAttestation, type ViemClient } from "../utils";
+
+import { abi as trustedOracleArbiterAbi } from "../contracts/TrustedOracleArbiter";
 
 export const makeOracleClient = (
   viemClient: ViemClient,
@@ -16,7 +20,6 @@ export const makeOracleClient = (
   return {
     listenAndArbitrate: async <
       T extends readonly AbiParameter[],
-      U extends readonly AbiParameter[],
       const abiEvent extends AbiEvent | undefined = undefined,
       const abiEvents extends
         | readonly AbiEvent[]
@@ -33,14 +36,43 @@ export const makeOracleClient = (
         fromBlock,
         toBlock
       >;
-      abis: { statement: T; demand: U };
-      getAttestationUidFromEvent: (event: abiEvent) => `0x${string}`;
+      statementAbi: T;
+      getAttestationUidFromEventLog: (
+        event: GetLogsReturnType<
+          abiEvent | undefined,
+          abiEvents | undefined,
+          strict,
+          fromBlock,
+          toBlock
+        >[number],
+      ) => `0x${string}`;
       arbitrate: (
         statement: DecodeAbiParametersReturnType<T>,
-        demand: DecodeAbiParametersReturnType<U>,
       ) => Promise<boolean | null>;
     }) => {
       const logs = await viemClient.getLogs(params.getLogsArgs);
+      const decisions = await Promise.all(
+        logs.map(async (log) => {
+          const attestationUid = params.getAttestationUidFromEventLog(log);
+          const attestation = await getAttestation(viemClient, attestationUid);
+
+          const statement = decodeAbiParameters(
+            params.statementAbi,
+            attestation.data,
+          );
+
+          const decision = await params.arbitrate(statement);
+          if (decision === null) return null;
+
+          const hash = await viemClient.writeContract({
+            address: addresses.trustedOracleArbiter,
+            abi: trustedOracleArbiterAbi.abi,
+            functionName: "arbitrate",
+            args: [attestationUid, decision],
+          });
+          return { hash, decision };
+        }),
+      );
     },
   };
 };
